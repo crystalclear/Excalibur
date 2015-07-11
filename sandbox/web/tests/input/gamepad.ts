17f0c75cc05c5c682bfdf8fb3c957d483f8ded61
Josh Edeen <josh.edeen@gmail.com>
2015-07-11 13:57:51 -0500

    fixed gamepad test

--- sandbox/web/tests/input/gamepad.ts
+++ sandbox/web/tests/input/gamepad.ts
@@ -97,7 +97,7 @@ class CircleActor extends ex.Actor {
       ctx.translate(this.x, this.y);
 
       ctx.beginPath();
-      ctx.arc(0, 0, this.getWidth(), 0, 2 * Math.PI, true);
+      ctx.arc(0, 0, this.width(), 0, 2 * Math.PI, true);
       ctx.fillStyle = this.color.toString();
       ctx.fill();
       ctx.closePath();
